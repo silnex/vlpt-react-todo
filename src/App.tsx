14a596f416d056ff71b9dbaf1dc52f52b65359d5
silnex <silnex@silnex.kr>
2022-12-13 00:13:16 +0900

    Make TodoCreate

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import TodoCreate from "./components/TodoCreate";
 import TodoHead from "./components/TodoHead";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
@@ -8,6 +9,7 @@ function App() {
       <TodoTemplate>
         <TodoHead />
         <TodoList />
+        <TodoCreate />
       </TodoTemplate>
     </>
   );
